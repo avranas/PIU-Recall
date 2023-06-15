f98fe7fa6e7d01f0271b0e591488c05dfea58684
Alex Vranas <avranas42@gmail.com>
2023-06-14 21:10:30 -0400

    Add login page

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
-//TODO NEXT: 
+//TODO NEXT:
 //Wireframe project
 
 function App() {
   const [serverResponse, setServerResponse] = useState();
+  const [usernameInput, setUsernameInput] = useState();
+  const [passwordInput, setPasswordInput] = useState();
+
   useEffect(() => {
     async function getHelloWorld() {
       const response = await fetch('/test');
@@ -15,10 +18,42 @@ function App() {
     }
     getHelloWorld();
   }, []);
+
+  function login() {
+    console.log(usernameInput)
+    console.log(passwordInput)
+    const body = {
+      username: usernameInput,
+      password: passwordInput
+    }
+    const response = fetch('/login', body);
+    console.log(response);
+    //TODO NEXT: Come back after I handle logging in
+    //Passport.js is next, then registering, then front end
+  }
   return (
     <div>
-      <h1>{serverResponse}</h1>
-      <h2>@@@</h2>
+      <h1>PIU Recall</h1>
+      <div id="username-input">
+        <label htmlFor="username">username: </label>
+        <input
+          type="text"
+          id="username"
+          onChange={(e) => setUsernameInput(e.target.value)}
+        ></input>
+      </div>
+      <div id="password-input">
+        <label htmlFor="password">password: </label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPasswordInput(e.target.value)}
+        ></input>
+      </div>
+      <button onClick={login}>Log in</button>
+      <footer>
+        <p>Made with ❤️ by Alex Vranas</p>
+      </footer>
     </div>
   );
 }
